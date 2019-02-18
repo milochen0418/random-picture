c7bd7357e9aa7df1e5176b33ff67bd5eca663677
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-18 21:42:49 +0800

    add ./set_new_random_picture.sh IDX to set new random picture on web

---
+++ set_new_random_picture.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ARG=1
+
+if [ $# -gt 0 ]; then
+	ARG=$1
+fi
+
+TOTAL=$(cat list.txt | wc -l)
+ARG=$((ARG % TOTAL + 1))
+#echo $ARG
+FILE_NAME=$(./query_list.sh $ARG);
+echo "FILE_NAME=$FILE_NAME"
+cp ./previews/$FILE_NAME ./public/images/index.jpg
