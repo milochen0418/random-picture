4f5603f23d0beded5bd94f31fc3e7d949fafe0ea
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-18 21:25:03 +0800

    execte command line in change web request

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,19 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/change', function(req, res, next) {
+  console.log("/change is called");
   res.render('index', { title: 'Express Change' });
-
+  const exec = require('child_process').exec;
+  //var command ='ls -alG ./ | wc -l';
+  var command ='echo $PWD';
+  const child = exec( command,
+    (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    });
 });
 
 
